47d704e5f2add5baf8dc71bdf7325b2b130815bb
Tim Saucer <timsaucer@gmail.com>
2025-01-07 12:59:42 -0500

    Cast data type based on expected schema

--- kernel/src/scan/partition_skipping.rs
+++ kernel/src/scan/partition_skipping.rs
@@ -5,7 +5,6 @@ use std::{
 
 use tracing::debug;
 
-use crate::expressions::column_expr;
 use crate::schema::column_name;
 use crate::{
     engine_data::GetData,
@@ -15,10 +14,12 @@ use crate::{
     schema::{ColumnName, DataType, MapType, SchemaRef},
     DeltaResult, Engine, EngineData, Expression, ExpressionEvaluator, ExpressionRef, RowVisitor,
 };
+use crate::expressions::column_expr;
 
 pub(crate) struct PartitionSkippingFilter {
     evaluator: Arc<dyn ExpressionEvaluator>,
     predicate: Arc<Expression>,
+    schema: SchemaRef,
 }
 
 impl PartitionSkippingFilter {
@@ -29,8 +30,9 @@ impl PartitionSkippingFilter {
         static PARITIONS_EXPR: LazyLock<Expression> =
             LazyLock::new(|| column_expr!("add.partitionValues"));
 
-        let (predicate, _referenced_schema) = physical_predicate?;
+        let (predicate, schema) = physical_predicate?;
         debug!("Creating a partition skipping filter for {:#?}", predicate);
+        println!("reference schema\n{:#?}", schema);
 
         let partitions_map_type = MapType::new(DataType::STRING, DataType::STRING, true);
 
@@ -43,6 +45,7 @@ impl PartitionSkippingFilter {
         Some(Self {
             evaluator,
             predicate,
+            schema,
         })
     }
 
@@ -50,7 +53,7 @@ impl PartitionSkippingFilter {
         let partitions = self.evaluator.evaluate(actions)?;
         assert_eq!(partitions.len(), actions.len());
 
-        let mut visitor = PartitionVisitor::new(&self.predicate);
+        let mut visitor = PartitionVisitor::new(&self.predicate, &self.schema);
         visitor.visit_rows_of(partitions.as_ref())?;
         Ok(visitor.selection_vector.clone())
     }
@@ -59,13 +62,15 @@ impl PartitionSkippingFilter {
 struct PartitionVisitor {
     pub(crate) selection_vector: Vec<bool>,
     predicate: Arc<Expression>,
+    schema: SchemaRef,
 }
 
 impl PartitionVisitor {
-    pub(crate) fn new(predicate: &Arc<Expression>) -> Self {
+    pub(crate) fn new(predicate: &Arc<Expression>, schema: &SchemaRef) -> Self {
         Self {
             selection_vector: Vec::default(),
             predicate: Arc::clone(predicate),
+            schema: Arc::clone(schema),
         }
     }
 }
@@ -90,15 +95,30 @@ impl RowVisitor for PartitionVisitor {
     fn visit<'a>(&mut self, row_count: usize, getters: &[&'a dyn GetData<'a>]) -> DeltaResult<()> {
         let getter = getters[0];
         for i in 0..row_count {
-            let val = getter.get_map(i, "output")?.map(|m| {
+            let val = getter.get_map(i, "output")?.and_then(|m| {
                 let partition_values = m.materialize();
                 // TODO(tsaucer) instead of casting to Scalar::String we need to use the appropriate schema
-                let resolver: HashMap<ColumnName, Scalar> = partition_values
+                let resolver = partition_values
                     .iter()
-                    .map(|(k, v)| (ColumnName::new([k]), Scalar::String(v.to_owned())))
-                    .collect();
+                    .map(|(k, v)| {
+                        let data_type = self.schema.field(k).map(|f| f.data_type());
+                        let primitve_type = if let Some(DataType::Primitive(primitive)) = data_type
+                        {
+                            primitive.to_owned()
+                        } else {
+                            return Err(crate::Error::Generic(
+                                "partition filtering only supported for primitive types"
+                                    .to_string(),
+                            ));
+                        };
+
+                        let scalar = primitve_type.parse_scalar(v)?;
+                        Ok((ColumnName::new([k]), scalar))
+                    })
+                    .collect::<DeltaResult<HashMap<ColumnName, Scalar>>>()
+                    .ok()?;
                 let filter = DefaultPredicateEvaluator::from(resolver);
-                filter.eval_expr(&self.predicate, false).unwrap_or(true)
+                Some(filter.eval_expr(&self.predicate, false).unwrap_or(true))
             });
 
             self.selection_vector.push(val.unwrap_or(true));
