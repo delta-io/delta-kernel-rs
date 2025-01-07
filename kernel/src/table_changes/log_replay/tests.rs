19cd74ef016b07e1a42b8c8609b518fcf9792d4e
Oussama Saoudi <oussama.saoudi@databricks.com>
2025-01-07 13:23:30 -0800

    remove todo comments

--- kernel/src/table_changes/log_replay/tests.rs
+++ kernel/src/table_changes/log_replay/tests.rs
@@ -259,8 +259,6 @@ async fn incompatible_schemas_fail() {
     ]);
     assert_schema_check(schema, get_schema(), false).await;
 
-    // Note: Once schema evolution is supported, this should not return an error.
-    //
     // The CDF schema has fields: nullable `id`  and nullable `value`.
     // This commit has schema with fields: non-nullable `id` and nullable `value`.
     let schema = StructType::new([
@@ -269,7 +267,6 @@ async fn incompatible_schemas_fail() {
     ]);
     assert_schema_check(schema, get_schema(), true).await;
 
-    // Note: Once schema evolution is supported, this should not return an error.
     // The CDF schema has fields: `id` (nullable) and `value` (nullable).
     // This commit has schema with fields: `id` (nullable).
     let schema = get_schema().project_as_struct(&["id"]).unwrap();
