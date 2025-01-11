2e071d98c7951673dd9a91e06355f2cb55795c80
Tim Saucer <timsaucer@gmail.com>
2025-01-11 10:29:54 -0500

    Add unit test for partition skipping

--- kernel/src/scan/partition_skipping.rs
+++ kernel/src/scan/partition_skipping.rs
@@ -131,3 +131,85 @@ impl RowVisitor for PartitionVisitor {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use arrow_array::{RecordBatch, StringArray};
+    use arrow_schema::{DataType as ArrowDataType, Field as ArrowField, Schema as ArrowSchema};
+    use std::sync::Arc;
+
+    use crate::engine::{arrow_data::ArrowEngineData, sync::SyncEngine};
+    use crate::expressions::UnaryOperator;
+    use crate::scan::get_log_schema;
+    use crate::schema::{DataType, Schema, StructField};
+    use crate::DeltaResult;
+    use crate::{DeltaResult, Engine, EngineData, Expression};
+
+    use super::PartitionSkippingFilter;
+
+    // TODO(nick): Merge all copies of this into one "test utils" thing
+    fn string_array_to_engine_data(string_array: StringArray) -> Box<dyn EngineData> {
+        let string_field = Arc::new(ArrowField::new("a", ArrowDataType::Utf8, true));
+        let schema = Arc::new(ArrowSchema::new(vec![string_field]));
+        let batch = RecordBatch::try_new(schema, vec![Arc::new(string_array)])
+            .expect("Can't convert to record batch");
+        Box::new(ArrowEngineData::new(batch))
+    }
+
+    #[test]
+    fn test_partition_skipping() -> DeltaResult<()> {
+        let engine = SyncEngine::new();
+        let json_handler = engine.get_json_handler();
+        let json_strings: StringArray = vec![
+            // All these values should be filtered due to c1 value
+            r#"{"add":{"path":"c1=4/c2=c/part-00003-f525f459-34f9-46f5-82d6-d42121d883fd.c000.snappy.parquet","partitionValues":{"c1":"1","c2":""},"size":452,"modificationTime":1670892998135,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":5},\"maxValues\":{\"c3\":5},\"nullCount\":{\"c3\":0}}"}}"#,
+            r#"{"add":{"path":"c1=4/c2=c/part-00003-f525f459-34f9-46f5-82d6-d42121d883fd.c000.snappy.parquet","partitionValues":{"c1":"2","c2":null},"size":452,"modificationTime":1670892998135,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":5},\"maxValues\":{\"c3\":5},\"nullCount\":{\"c3\":0}}"}}"#,
+            r#"{"add":{"path":"c1=4/c2=c/part-00003-f525f459-34f9-46f5-82d6-d42121d883fd.c000.snappy.parquet","partitionValues":{"c1":"3","c2":"a"},"size":452,"modificationTime":1670892998135,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":5},\"maxValues\":{\"c3\":5},\"nullCount\":{\"c3\":0}}"}}"#,
+
+            // Test both null and "" produce valid nulls
+            r#"{"add":{"path":"c1=4/c2=c/part-00003-f525f459-34f9-46f5-82d6-d42121d883fd.c000.snappy.parquet","partitionValues":{"c1":"4","c2":""},"size":452,"modificationTime":1670892998135,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":5},\"maxValues\":{\"c3\":5},\"nullCount\":{\"c3\":0}}"}}"#,
+            r#"{"add":{"path":"c1=5/c2=b/part-00007-4e73fa3b-2c88-424a-8051-f8b54328ffdb.c000.snappy.parquet","partitionValues":{"c1":"5","c2":null},"size":452,"modificationTime":1670892998136,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":6},\"maxValues\":{\"c3\":6},\"nullCount\":{\"c3\":0}}"}}"#,
+            r#"{"add":{"path":"c1=6/c2=a/part-00011-10619b10-b691-4fd0-acc4-2a9608499d7c.c000.snappy.parquet","partitionValues":{"c1":"6","c2":"b"},"size":452,"modificationTime":1670892998137,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":4},\"maxValues\":{\"c3\":4},\"nullCount\":{\"c3\":0}}"}}"#,
+
+            // Gracefully handle missing partition values as null
+            r#"{"add":{"path":"c1=4/c2=c/part-00003-f525f459-34f9-46f5-82d6-d42121d883fd.c000.snappy.parquet","partitionValues":{"c1":"1"},"size":452,"modificationTime":1670892998135,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":5},\"maxValues\":{\"c3\":5},\"nullCount\":{\"c3\":0}}"}}"#,
+            r#"{"add":{"path":"c1=5/c2=b/part-00007-4e73fa3b-2c88-424a-8051-f8b54328ffdb.c000.snappy.parquet","partitionValues":{"c1":"5"},"size":452,"modificationTime":1670892998136,"dataChange":true,"stats":"{\"numRecords\":1,\"minValues\":{\"c3\":6},\"maxValues\":{\"c3\":6},\"nullCount\":{\"c3\":0}}"}}"#,
+
+            ]
+        .into();
+
+        let log_schema = get_log_schema().clone();
+        let batch = json_handler
+            .parse_json(
+                string_array_to_engine_data(json_strings),
+                log_schema.clone(),
+            )
+            .unwrap();
+
+        let expr = Arc::new(Expression::and(
+            Expression::unary(UnaryOperator::IsNull, Expression::column(["c2"])),
+            Expression::ge(Expression::column(["c1"]), Expression::literal(4)),
+        ));
+
+        let schema = Arc::new(Schema::new(vec![
+            StructField::new("c1", DataType::INTEGER, true),
+            StructField::new("c2", DataType::STRING, true),
+            StructField::new("c3", DataType::INTEGER, true),
+        ]));
+
+        let physical_predicate = Some((expr, schema));
+        let filter = PartitionSkippingFilter::new(
+            &engine,
+            physical_predicate,
+            &["c1".to_string(), "c2".to_string()],
+        )
+        .expect("Unable to create Partition Skipping Filter");
+
+        let actual = filter.apply(batch.as_ref())?;
+
+        let expected = vec![false, false, false, true, true, false, false, true];
+
+        assert_eq!(actual, expected);
+        Ok(())
+    }
+}
