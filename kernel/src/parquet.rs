d737dfad5babd6d7c693ca40b0f6d94eaabf51b6
R. Tyler Croy <rtyler@brokenco.de>
2025-01-25 18:49:50 +0000

    chore: rework the arrow integration tests
    This now relies on the features explicitly supported by the kernel crate
    rather than released arrow versions which may or may not be supported

--- kernel/src/parquet.rs
+++ kernel/src/parquet.rs
@@ -1,4 +1,4 @@
-//! This module exists to help re-export the version of arrow used by default-gengine and other
+//! This module exists to help re-export the version of arrow used by default-engine and other
 //! parts of kernel that need arrow
 
 #[cfg(all(feature = "arrow_53", feature = "arrow_54"))]
