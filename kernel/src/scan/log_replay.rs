e11afadd5d471eea4eaa0501212292f47e29f3b6
Tim Saucer <timsaucer@gmail.com>
2025-01-26 10:40:08 -0500

    Move checking for empty partitions into the partition skipping filter rather than assuming the caller knows what they're doing

--- kernel/src/scan/log_replay.rs
+++ kernel/src/scan/log_replay.rs
@@ -258,13 +258,10 @@ impl LogReplayScanner {
         physical_predicate: Option<(ExpressionRef, SchemaRef)>,
         partition_columns: &[String],
     ) -> Self {
-        let partition_predicate = physical_predicate
-            .as_ref()
-            .filter(|_| !partition_columns.is_empty());
         Self {
             partition_filter: PartitionSkippingFilter::new(
                 engine,
-                partition_predicate.cloned(),
+                physical_predicate.clone(),
                 partition_columns,
             ),
             data_filter: DataSkippingFilter::new(engine, physical_predicate),
