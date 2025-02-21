85ebcb89fa575663c68084e5eb488b2c404bc9a6
Nick Lanham <nick@databricks.com>
2025-02-21 10:25:33 -0800

    also for parquet.rs

--- kernel/src/parquet.rs
+++ kernel/src/parquet.rs
@@ -1,10 +1,7 @@
 //! This module exists to help re-export the version of arrow used by default-engine and other
 //! parts of kernel that need arrow
 
-#[cfg(all(feature = "arrow_53", feature = "arrow_54"))]
-pub use parquet_53::*;
-
-#[cfg(all(feature = "arrow_53", not(feature = "arrow_54")))]
+#[cfg(feature = "arrow_53")]
 pub use parquet_53::*;
 
 #[cfg(all(feature = "arrow_54", not(feature = "arrow_53")))]
