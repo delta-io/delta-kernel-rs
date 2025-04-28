6ba9dfc19f6b0b8f926bb30ae10c727b360b4f61
Zach Schuermann <zachary.zvs@gmail.com>
2025-04-28 10:49:45 -0700

    arrow and panic fixes

--- kernel/src/arrow_compat.rs
+++ kernel/src/arrow_compat.rs
@@ -1,13 +1,6 @@
 //! This module re-exports the different versions of arrow, parquet, and object_store we support.
 
-#[cfg(all(feature = "arrow-55", not(feature = "arrow-54")))]
-mod arrow_compat_shims {
-    pub use arrow_55 as arrow;
-    pub use object_store_55 as object_store;
-    pub use parquet_55 as parquet;
-}
-
-#[cfg(all(feature = "arrow-55", feature = "arrow-54"))]
+#[cfg(feature = "arrow-55")]
 mod arrow_compat_shims {
     pub use arrow_55 as arrow;
     pub use object_store_55 as object_store;
