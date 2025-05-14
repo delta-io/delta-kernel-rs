6a45a5e2763b8545d1d2c7632d2a2efd383322c2
Robert Pack <robstar.pack@gmail.com>
2025-05-14 21:09:01 +0200

    chore: remove datafusiuon code

--- datafusion/src/error.rs
+++
@@ -1,6 +0,0 @@
-use datafusion_common::DataFusionError;
-use delta_kernel::error::Error as DeltaKernelError;
-
-pub(crate) fn to_df_err(e: DeltaKernelError) -> DataFusionError {
-    DataFusionError::External(Box::new(e))
-}
