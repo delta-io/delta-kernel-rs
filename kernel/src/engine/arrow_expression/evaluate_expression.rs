131b25ea5a1d7bfc1ce3ef1e4a03f266982480ee
Ryan Johnson <scovich@gmail.com>
2025-09-02 14:56:39 -0700

    code comment

--- kernel/src/engine/arrow_expression/evaluate_expression.rs
+++ kernel/src/engine/arrow_expression/evaluate_expression.rs
@@ -177,6 +177,7 @@ fn evaluate_transform_expression(
         ));
     }
 
+    // Verify that the lengths match before attempting to zip them below.
     if output_cols.len() != output_schema.fields_len() {
         return Err(Error::generic(format!(
             "Expression count ({}) doesn't match output schema field count ({})",
