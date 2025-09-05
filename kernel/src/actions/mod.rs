e9ae969eed7aad757dc2d4ecdf2e018db482127e
Mann Patel <mann.patel@databricks.com>
2025-09-05 21:49:28 +0000

    fix test

--- kernel/src/actions/mod.rs
+++ kernel/src/actions/mod.rs
@@ -602,7 +602,7 @@ impl CommitInfo {
             operation_parameters: None,
             kernel_version: Some(format!("v{KERNEL_VERSION}")),
             engine_info,
-            txn_id: uuid::Uuid::new_v4().to_string(),
+            txn_id: Some(uuid::Uuid::new_v4().to_string()),
         }
     }
 }
@@ -1393,6 +1393,7 @@ mod tests {
         let engine = ExprEngine::new();
 
         let commit_info = CommitInfo::new(0, None, None);
+        let commit_info_txn_id = commit_info.txn_id.clone();
 
         let engine_data = commit_info.into_engine_data(CommitInfo::to_schema().into(), &engine);
 
@@ -1417,6 +1418,7 @@ mod tests {
                 Arc::new(StringArray::from(vec![Some(format!("v{KERNEL_VERSION}"))])),
                 Arc::new(StringArray::from(vec![None::<String>])),
                 Arc::new(StringArray::from(vec![None::<String>])),
+                Arc::new(StringArray::from(vec!(commit_info_txn_id)))
             ],
         )
         .unwrap();
