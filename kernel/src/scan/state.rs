779d5532e8fc57f50dc556af5ce71f86bc7ce616
rachelyuan-db <rachel.yuan@databricks.com>
2025-10-09 22:21:30 +0000

    make public

--- kernel/src/scan/state.rs
+++ kernel/src/scan/state.rs
@@ -24,7 +24,7 @@ use super::ScanMetadata;
 /// this struct can be used by an engine to materialize a selection vector
 #[derive(Default, Debug, Clone, PartialEq, Eq)]
 pub struct DvInfo {
-    deletion_vector: Option<DeletionVectorDescriptor>,
+    pub deletion_vector: Option<DeletionVectorDescriptor>,
 }
 
 impl From<DeletionVectorDescriptor> for DvInfo {
