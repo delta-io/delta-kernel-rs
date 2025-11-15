3e5083e65d24e43358011e1296d2b5ffc3cc810f
Oussama Saoudi <oussama.sa216@gmail.com>
2025-11-14 17:29:15 -0800

    add check in transaction commit

--- kernel/src/transaction/mod.rs
+++ kernel/src/transaction/mod.rs
@@ -26,7 +26,7 @@ use crate::scan::log_replay::{
 use crate::scan::scan_row_schema;
 use crate::schema::{ArrayType, MapType, SchemaRef, StructField, StructType};
 use crate::snapshot::SnapshotRef;
-use crate::utils::current_time_ms;
+use crate::utils::{current_time_ms, require};
 use crate::{
     DataType, DeltaResult, Engine, EngineData, Expression, ExpressionRef, IntoEngineData,
     RowVisitor, SchemaTransform, Version,
@@ -232,6 +232,30 @@ impl Transaction {
                 dup.app_id
             )));
         }
+
+        // If there are add and remove files with data change in the same transaction, we block it.
+        // This is because kernel does not yet have a way to discern DML operations. For DML
+        // operations that perform updates on rows, ChangeDataFeed requires that a `cdc` file be
+        // written to the delta log.
+        if !self.add_files_metadata.is_empty()
+            && !self.remove_files_metadata.is_empty()
+            && self.data_change
+        {
+            let cdf_enabled = self
+                .read_snapshot
+                .table_configuration()
+                .table_properties()
+                .enable_change_data_feed
+                .unwrap_or(false);
+            require!(
+                !cdf_enabled,
+                Error::generic(
+                    "Cannot add and remove data in the same transaction
+                    if the table property 'delta.enableChangeDataFeed' = true."
+                )
+            );
+        }
+
         // Step 1: Generate SetTransaction actions
         let set_transaction_actions = self
             .set_transactions
