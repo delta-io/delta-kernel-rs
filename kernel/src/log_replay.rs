cb2a8c4f06d8aa412cc649348f6467b15bae2a9b
Oussama Saoudi <oussama.sa216@gmail.com>
2025-11-24 14:56:23 -0800

    executor

--- kernel/src/log_replay.rs
+++ kernel/src/log_replay.rs
@@ -21,6 +21,7 @@ use crate::{DeltaResult, EngineData};
 use delta_kernel_derive::internal_api;
 
 use std::collections::HashSet;
+use std::sync::Arc;
 
 use tracing::debug;
 
@@ -205,9 +206,20 @@ impl ActionsBatch {
 
 #[internal_api]
 #[allow(unused)]
-pub(crate) trait ParallelizableLogReplayProcessor {
+pub(crate) trait ParallelLogReplayProcessor {
     type Output;
     fn process_actions_batch(&self, actions_batch: ActionsBatch) -> DeltaResult<Self::Output>;
+}
+
+impl<T> ParallelLogReplayProcessor for Arc<T>
+where
+    T: ParallelLogReplayProcessor,
+{
+    type Output = T::Output;
+
+    fn process_actions_batch(&self, actions_batch: ActionsBatch) -> DeltaResult<Self::Output> {
+        self.as_ref().process_actions_batch(actions_batch)
+    }
 }
 
 /// A trait for processing batches of actions from Delta transaction logs during log replay.
