f77f06af800c92fc9827e4e0941fc0bceb43bc75
Oussama Saoudi <oussama.sa216@gmail.com>
2025-12-01 10:29:39 -0800

    make dedup compatible

--- kernel/src/log_replay/deduplicator.rs
+++ kernel/src/log_replay/deduplicator.rs
@@ -10,7 +10,12 @@
 //!
 //! [`FileActionDeduplicator`]: crate::log_replay::FileActionDeduplicator
 
-use crate::{engine_data::GetData, log_replay::FileActionKey, DeltaResult};
+use crate::{
+    actions::deletion_vector::DeletionVectorDescriptor,
+    engine_data::{GetData, TypedGetData as _},
+    log_replay::FileActionKey,
+    DeltaResult,
+};
 
 pub(crate) trait Deduplicator {
     /// Key type for identifying file actions. JSON deduplicators use `FileActionKey`
@@ -37,22 +42,25 @@ pub(crate) trait Deduplicator {
 pub(crate) struct CheckpointDeduplicator<'a> {
     seen_file_keys: &'a HashSet<FileActionKey>,
     add_path_index: usize,
+    add_dv_start_index: usize,
 }
 impl CheckpointDeduplicator<'_> {
     #[allow(unused)]
     pub(crate) fn try_new<'a>(
         seen_file_keys: &'a HashSet<FileActionKey>,
         add_path_index: usize,
+        add_dv_start_index: usize,
     ) -> DeltaResult<CheckpointDeduplicator<'a>> {
         Ok(CheckpointDeduplicator {
             seen_file_keys,
             add_path_index,
+            add_dv_start_index,
         })
     }
 }
 
 impl Deduplicator for CheckpointDeduplicator<'_> {
-    type Key = String;
+    type Key = FileActionKey;
 
     fn extract_file_action<'a>(
         &self,
@@ -62,7 +70,8 @@ impl Deduplicator for CheckpointDeduplicator<'_> {
     ) -> DeltaResult<Option<(Self::Key, bool)>> {
         // Try to extract an add action by the required path column
         if let Some(path) = getters[self.add_path_index].get_str(i, "add.path")? {
-            Ok(Some((path.to_string(), true)))
+            let dv_unique_id = extract_dv_unique_id(i, getters, self.add_dv_start_index)?;
+            Ok(Some((FileActionKey::new(path, dv_unique_id), true)))
         } else {
             Ok(None)
         }
@@ -74,5 +83,33 @@ impl Deduplicator for CheckpointDeduplicator<'_> {
 
     fn is_log_batch(&self) -> bool {
         false
+    }
+}
+
+/// Extracts the deletion vector unique ID if it exists.
+///
+/// This function retrieves the necessary fields for constructing a deletion vector unique ID
+/// by accessing `getters` at `dv_start_index` and the following two indices. Specifically:
+/// - `dv_start_index` retrieves the storage type (`deletionVector.storageType`).
+/// - `dv_start_index + 1` retrieves the path or inline deletion vector (`deletionVector.pathOrInlineDv`).
+/// - `dv_start_index + 2` retrieves the optional offset (`deletionVector.offset`).
+pub(crate) fn extract_dv_unique_id<'a>(
+    i: usize,
+    getters: &[&'a dyn GetData<'a>],
+    dv_start_index: usize,
+) -> DeltaResult<Option<String>> {
+    match getters[dv_start_index].get_opt(i, "deletionVector.storageType")? {
+        Some(storage_type) => {
+            let path_or_inline =
+                getters[dv_start_index + 1].get(i, "deletionVector.pathOrInlineDv")?;
+            let offset = getters[dv_start_index + 2].get_opt(i, "deletionVector.offset")?;
+
+            Ok(Some(DeletionVectorDescriptor::unique_id_from_parts(
+                storage_type,
+                path_or_inline,
+                offset,
+            )))
+        }
+        None => Ok(None),
     }
 }
