64818618bfae86a6cf70c9138d193829c3f5944a
Fokko Driesprong <fokko@apache.org>
2025-12-09 14:22:14 +0100

    Add comment

--- kernel/src/error.rs
+++ kernel/src/error.rs
@@ -344,6 +344,10 @@ impl From<object_store::Error> for Error {
     }
 }
 
+/// This impl is needed so the `?` operator can auto-convert `Result<T, Infallible>` to
+/// `DeltaResult<T>`. For example, `TryFrom` impls for infallible conversions use `Infallible` as
+/// their error type, and this allows those results to be propagated with `?` in functions
+/// returning `DeltaResult`. The match is unreachable since `Infallible` has no variants.
 impl From<Infallible> for Error {
     fn from(value: Infallible) -> Self {
         match value {}
