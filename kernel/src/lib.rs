6550ae0440d6dbfa2e1929a8089f1849e355a0b7
songhang deng <songhang.deng@databricks.com>
2025-12-23 01:32:23 +0000

    comments

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -876,5 +876,10 @@ compile_error!(
     Please use either default-engine-native-tls or default-engine-rustls."
 );
 
+// Rustdoc's documentation tests can do some things that regular unit tests can't. Here we are
+// using doctests to test macros. Specifically, we are testing for failed macro invocations due
+// to invalid input, not the macro output when the macro invocation is successful (which can/should be
+// done in unit tests). This module is not exclusively for macro tests only so other doctests can also be added.
+// https://doc.rust-lang.org/rustdoc/write-documentation/documentation-tests.html#include-items-only-when-collecting-doctests
 #[cfg(doctest)]
 mod doctests;
