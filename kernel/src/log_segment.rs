db991a8f67f8ee4b4b0f201ff447b81090817712
Scott Sandre <scott.sandre@databricks.com>
2026-01-09 22:52:36 +0000

    add LogSegment::new_with_commit

--- kernel/src/log_segment.rs
+++ kernel/src/log_segment.rs
@@ -327,6 +327,40 @@ impl LogSegment {
         LogSegment::try_new(listed_commits, log_root, Some(end_version), None)
     }
 
+    /// Creates a new LogSegment with the given commit file added to the end.
+    fn new_with_commit(self, tail_commit_file: ParsedLogPath) -> DeltaResult<Self> {
+        require!(
+            tail_commit_file.is_commit(),
+            Error::internal_error(format!(
+                "Cannot extend and create new LogSegment. Tail log file is not a commit file. \
+                Path: {}, Type: {:?}.",
+                tail_commit_file.location.location, tail_commit_file.file_type
+            ))
+        );
+        require!(
+            tail_commit_file.version == self.end_version + 1,
+            Error::internal_error(format!(
+                "Cannot extend and create new LogSegment. Tail commit file version ({}) does not \
+                equal LogSegment end_version ({}) + 1.",
+                tail_commit_file.version, self.end_version
+            ))
+        );
+
+        let new_end_version = tail_commit_file.version;
+
+        let mut new_ascending_commit_files = self.ascending_commit_files.clone();
+        new_ascending_commit_files.push(tail_commit_file.clone());
+
+        // TODO: calculate new max published version, when applicable
+
+        Ok(LogSegment {
+            end_version: new_end_version,
+            ascending_commit_files: new_ascending_commit_files,
+            latest_commit_file: Some(tail_commit_file),
+            ..self
+        })
+    }
+
     /// Read a stream of actions from this log segment. This returns an iterator of
     /// [`ActionsBatch`]s which includes EngineData of actions + a boolean flag indicating whether
     /// the data was read from a commit file (true) or a checkpoint file (false).
