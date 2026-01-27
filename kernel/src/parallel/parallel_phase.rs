125a05ce1445ec499d51b4cbe5dcf385018418a7
Oussama Saoudi <oussama.sa216@gmail.com>
2026-01-27 10:23:26 -0800

    address comment

--- kernel/src/parallel/parallel_phase.rs
+++ kernel/src/parallel/parallel_phase.rs
@@ -113,9 +113,9 @@ impl<P: ParallelLogReplayProcessor> Iterator for ParallelPhase<P> {
     type Item = DeltaResult<P::Output>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.leaf_checkpoint_reader.next().map(|batch_res| {
-            batch_res.and_then(|batch| self.processor.process_actions_batch(batch))
-        })
+        self.leaf_checkpoint_reader
+            .next()
+            .map(|batch| self.processor.process_actions_batch(batch?))
     }
 }
 
