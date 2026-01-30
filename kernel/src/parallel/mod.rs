319989055772e952fa03e27c1f5988b9fce940c2
Oussama Saoudi <oussama.sa216@gmail.com>
2026-01-29 20:11:00 -0800

    internal-api

--- kernel/src/parallel/mod.rs
+++ kernel/src/parallel/mod.rs
@@ -1,7 +1,23 @@
 //! Two-phase log replay for parallel execution of checkpoint processing.
 
+use crate::internal_api;
+
+#[cfg(feature = "internal-api")]
+pub mod parallel_phase;
+#[cfg(not(feature = "internal-api"))]
 pub(crate) mod parallel_phase;
+
+#[cfg(feature = "internal-api")]
+pub mod sequential_phase;
+#[cfg(not(feature = "internal-api"))]
 pub(crate) mod sequential_phase;
 
+#[cfg(feature = "internal-api")]
+pub use parallel_phase::ParallelPhase;
+#[cfg(not(feature = "internal-api"))]
 pub(crate) use parallel_phase::ParallelPhase;
+
+#[cfg(feature = "internal-api")]
+pub use sequential_phase::{AfterSequential, SequentialPhase};
+#[cfg(not(feature = "internal-api"))]
 pub(crate) use sequential_phase::{AfterSequential, SequentialPhase};
