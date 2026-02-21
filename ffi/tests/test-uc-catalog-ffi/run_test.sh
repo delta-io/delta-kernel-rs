8876556d03b85031d2e82b26d86023729a140110
Nick Lanham <nick@databricks.com>
2026-02-20 16:16:33 -0800

    okay, osx for reals

--- ffi/tests/test-uc-catalog-ffi/run_test.sh
+++ ffi/tests/test-uc-catalog-ffi/run_test.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-OUT_FILE=$(mktemp)
-TMP_TABLE_DIR=$(mktemp -d)
-cp -r ../../../../kernel/tests/data/table-with-dv-small/ "$TMP_TABLE_DIR"
+OUT_FILE=$(mktemp "${TMPDIR:-/tmp}/catalog_test.out.XXXX")
+TMP_TABLE_DIR=$(mktemp -d "${TMPDIR:-/tmp}/catalog_test.table.XXXX")
+cp -r ../../../../kernel/tests/data/table-with-dv-small "$TMP_TABLE_DIR"
 
 ./uc_catalog_example "$TMP_TABLE_DIR/table-with-dv-small"
 CATALOG_EXIT_CODE=$?
