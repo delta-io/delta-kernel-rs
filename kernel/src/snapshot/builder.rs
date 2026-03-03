f991ca9fde7d78a32f298f3f1c9af2c2b17d4e7e
Jameson Crate <jameson.crate@databricks.com>
2026-03-03 00:39:06 +0000

    add issue numbers to todos

--- kernel/src/snapshot/builder.rs
+++ kernel/src/snapshot/builder.rs
@@ -181,7 +181,7 @@ mod tests {
         (engine, store, table_root)
     }
 
-    // TODO: update this function to properly store the table at table_root
+    // TODO (#1990): update this function to properly store the table at table_root
     async fn create_table(store: &Arc<dyn ObjectStore>, _table_root: String) -> DeltaResult<()> {
         let protocol = json!({
             "minReaderVersion": 3,
