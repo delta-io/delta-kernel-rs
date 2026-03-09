332591751f6d9ad93a2b4bea5dfe3186b528dc8f
Thinh Bui <thinhbui.work@gmail.com>
2026-03-09 17:50:08 +0000

    fix return error

--- kernel/src/transaction/commit_info.rs
+++ kernel/src/transaction/commit_info.rs
@@ -57,8 +57,8 @@ fn commit_info_literal_exprs(
     ];
     let expected_expr_len = CommitInfo::to_schema().fields().len();
     if literal_exprs.len() != expected_expr_len {
-        Error::Generic(format!("expect the commit_info_literal_exprs return {expected_expr_len} expressions, but only get {} expressions. 
-            If CommitInfo field was added/removed, please update Expression::Literal in this function and update the with_commit_info doc comment", literal_exprs.len()));
+        return Err(Error::Generic(format!("expect the commit_info_literal_exprs return {expected_expr_len} expressions, but only get {} expressions. \
+            If CommitInfo field was added/removed, please update Expression::Literal in this function and update the with_commit_info doc comment", literal_exprs.len())));
     }
     Ok(literal_exprs)
 }
