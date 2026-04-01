adfe87104f21e148be5c49ff3a369bcf6411c9f4
Ryan Liao <ryanliao123@gmail.com>
2026-04-01 23:03:35 +0000

    chore: fix import ordering in utils/mod.r

--- delta-kernel-unity-catalog/src/utils/mod.rs
+++ delta-kernel-unity-catalog/src/utils/mod.rs
@@ -1,3 +1,3 @@
 pub(crate) mod create_table;
 
-pub use create_table::{get_required_properties_for_disk, get_final_required_properties_for_uc};
+pub use create_table::{get_final_required_properties_for_uc, get_required_properties_for_disk};
