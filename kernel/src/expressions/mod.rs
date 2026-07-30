b799c27a6bdf1b85e2438163c8f29505fd7e161a
Oussama Saoudi <oussama.sa216@gmail.com>
2026-07-30 22:24:32 +0000

    refactor: encapsulate metadata scan plan construction

--- kernel/src/expressions/mod.rs
+++ kernel/src/expressions/mod.rs
@@ -1207,6 +1207,12 @@ impl From<Predicate> for Expression {
     }
 }
 
+impl From<Predicate> for Option<PredicateRef> {
+    fn from(value: Predicate) -> Self {
+        Some(Arc::new(value))
+    }
+}
+
 impl From<ColumnName> for Predicate {
     fn from(value: ColumnName) -> Self {
         Self::from_expr(value)
