bf1061dda2b592308337d154cef221bdaca2b329
Amogh Jahagirdar <amoghj@apache.org>
2026-08-17 13:54:46 -0600

    one pass

--- derive-macros/src/lib.rs
+++ derive-macros/src/lib.rs
@@ -467,23 +467,21 @@ pub fn into_struct_data_derive(input: proc_macro::TokenStream) -> proc_macro::To
         Ok(fields) => fields,
         Err(e) => return e.to_compile_error().into(),
     };
-    let field_types: Vec<_> = fields.iter().map(|f| &f.ty).collect();
-    // Match `ToSchema`: an `#[allow_null_container_values]` field's value must be value-nullable
-    // too.
-    let field_values: Vec<_> = fields
+
+    let (field_types, field_values): (Vec<_>, Vec<_>) = fields
         .iter()
-        .map(|f| {
-            let ident = &f.ident;
-            if has_named_attr(&f.attrs, "allow_null_container_values") {
+        .map(|f: &&Field| {
+            let ident: &Option<Ident> = &f.ident;
+            let value: TokenStream = if has_named_attr(&f.attrs, "allow_null_container_values") {
                 quote! {
-                    delta_kernel::expressions::Scalar::from(value.#ident)
-                        .with_nullable_container_values()
+                    delta_kernel::expressions::Scalar::from(value.#ident).with_nullable_container_values()
                 }
             } else {
-                quote! { value.#ident.into() }
-            }
+                quote! { value.#ident.into()}
+            };
+            (&f.ty, value)
         })
-        .collect();
+        .unzip();
 
     let expanded = quote! {
         #[automatically_derived]
